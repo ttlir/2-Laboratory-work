1a302c7fdfa9def5ef11f4d70db33eea69550744
ttlir <157885296+ttlir@users.noreply.github.com>
2024-01-30 15:38:44 +0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,93 @@
+#Ex1
+print("Hello, World!")
+
+#Ex2
+if 5 > 2:
+    print("YES")
+
+#Ex3    
+#This is a coment 
+
+#Ex4
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+#Ex5
+carname= "Volvo"
+
+
+#Ex6
+x = 50
+
+#Ex7
+x=5
+y=10
+ print(x+y)
+
+#Ex8
+x=5
+y=10
+z=x+y
+ print(z)
+
+
+#Ex9
+x, y, z= "Orange", "Banana", "Cherry"
+
+#Ex10
+x = y = z = "Orange"
+
+#Ex11
+def myfunc()
+  global x
+  x= "fantastic"
+    
+#Ex12
+x = 5
+print(type(x))
+int
+
+#Ex13
+x= "Hello World"
+print(type(x))
+str
+
+#Ex14
+x=20.5
+print(type(x))
+float
+
+#Ex15
+x={"apple", "banana", "cherry"}
+print(type(x))
+list
+
+#Ex16
+x=("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#Ex17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#Ex18
+x = True
+print(type(x))
+bool
+
+#Ex19
+x=5
+x=float(x)
+
+#Ex20
+x=5.5
+x=int(x)
+
+#Ex21
+x=5
+x=complex(x)
